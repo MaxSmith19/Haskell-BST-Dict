d7e2b9fd192ce5473f5605f0a8dc0506d9f4dc62
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-23 21:26:50 +0100

    git init

---
+++ src/BST.hs
@@ -0,0 +1,16 @@
+module BST where
+
+-- A dictionary data structure contains a collection of key/item pairs, such that each key occurs at-most once,
+-- but the same item may occur several times. Key/item pairs are known as entries.
+
+-- Four basic dictionary operations are:
+
+--     create an empty dictionary;
+--     insert an entry;
+--     lookup the item by specifying a key;
+--     produce an ordered list of all entries (as pairs in a list data structure).
+
+-- More advanced dictionary operations are:
+
+--     remove an entry by specifying the key;
+--     remove all entries that satisfy a specified predicate function
---
+++ test/BinarySearchTreeTests.hs
@@ -0,0 +1,5 @@
+module BinarySearchTreeTests where
+
+import Test.HUnit
+
+-- test for an empty binary search tree
