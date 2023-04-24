5fa2acc8db4056eb188afb1a95fdc11b241af5ab
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-24 13:41:01 +0100

    testTreeWithNodes failing (Non-compiling)

--- test/BinarySearchTreeTests.hs
+++ test/BinarySearchTreeTests.hs
@@ -10,9 +10,23 @@ main = do
 
 
 allTests :: Test
-allTests = TestList [testEmptyTree]
+allTests = TestList [
+    testEmptyTree,
+    testTreeWithNodes
+    ]
 
 
 -- test for a binary search tree
 testEmptyTree :: Test
-testEmptyTree = TestCase (assertEqual "Creates an empty tree" Empty (emptyBST :: BST Int Int))
+testEmptyTree = TestCase (assertEqual "Creates an empty tree" Empty (emptyBST :: BST Int String))
+
+--test for a tree with nodes 
+testTreeWithNodes :: Test
+testTreeWithNodes = TestCase (assertEqual "Creates a tree with nodes" expectedTree (treeWithNodes :: BST Int String))
+    where 
+        treeWithNodes = Node 2 "B"
+                        (Node 1 "A" Empty Empty) -- left
+                        (Node 3 "C" Empty Empty) -- right
+        expectedTree = Node 2 "B"
+                        (Node 1 "A" Empty Empty) -- left
+                        (Node 3 "C" Empty Empty) -- right
