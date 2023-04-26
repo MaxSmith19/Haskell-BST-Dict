190d9b369e707c07ab30b2b471189fcc0c888746
Max Smith <47494186+MaxSmith19@users.noreply.github.com>
2023-04-26 17:41:32 +0100

    BST made polymorphic + stub for left rotation

--- src/BST.hs
+++ src/BST.hs
@@ -1,4 +1,14 @@
-module BST where
+module BST (
+    BST(..),
+    emptyBST,
+    insert,
+    treeLookup,
+    treeToList,
+    remove,
+    removeIf,
+    findMin,
+    treeHeight
+) where
 
 -- A dictionary data structure contains a collection of key/item pairs, such that each key occurs at-most once,
 -- but the same item may occur several times. Key/item pairs are known as entries.
@@ -24,14 +34,14 @@ data BST key value = Empty
 -- deriving is a method of polymorphism, 
 -- it allows us to use the same function for different types, we want to be able to compare two BSTs
 
--- create an empty dictionary
-emptyBST :: BST Int String
+-- create an empty tree
+emptyBST :: BST key value
 emptyBST = Empty
 -- as we defined a binary search tree as either Empty or Node,
 -- we can create an empty binary search tree by simply returning an "Empty" tree
 
 --insert an entry 
-insert :: Int -> String -> BST Int String -> BST Int String
+insert :: (Ord key) => key -> value -> BST key value -> BST key value
 insert k v Empty = Node k v Empty Empty -- if empty, create a new node
 insert k v (Node keyToAdd valueToAdd left right) -- if not empty, add to the tree
     | k == keyToAdd = Node keyToAdd v left right
@@ -39,7 +49,7 @@ insert k v (Node keyToAdd valueToAdd left right) -- if not empty, add to the tre
     | k > keyToAdd = Node keyToAdd valueToAdd left (insert k v right)
 
 --lookup the item by specifying a key
-treeLookup :: Int -> BST Int String -> Bool
+treeLookup :: (Ord key) => key -> BST key value -> Bool
 treeLookup k Empty = False -- if empty, return false
 treeLookup k (Node keyToFind valueToFind left right) 
     | k == keyToFind = True
@@ -47,7 +57,7 @@ treeLookup k (Node keyToFind valueToFind left right)
     | k > keyToFind = treeLookup k right
 
 -- produce an ordered list of all entries (as pairs in a list data structure)
-treeToList :: BST Int String -> [(Int, String)]
+treeToList :: (Ord key) => BST key value -> [(key, value)]
 treeToList Empty = []
 treeToList (Node key value left right) = 
     let 
@@ -57,7 +67,7 @@ treeToList (Node key value left right) =
         leftList ++ [(key, value)] ++ rightList
         
 -- remove an entry from the tree by specifying the key
-remove :: Int -> BST Int String -> BST Int String
+remove :: (Ord key) => key -> BST key value -> BST key value
 remove k Empty = Empty
 remove k (Node key value left right)
     | k < key = Node key value (remove k left) right
@@ -70,20 +80,28 @@ remove k (Node key value left right)
             where -- if both are not empty, replace the node with the minimum value of the right subtree
                 (minKey, minValue) = findMin right
 
-findMin :: BST k v -> (k, v) -- find the minimum value of the tree
+findMin :: (Ord key) => BST key value -> (key, value)-- find the minimum value of the tree
 findMin (Node k v Empty _) = (k, v) -- if the left node is empty, return the current node
 findMin (Node _ _ left _) = findMin left -- if the left node is not empty, recursively go to the left node
 
 -- remove all entries that satisfy a specified predicate function
 -- like being even or odd
-removeIf :: (Int -> Bool) -> BST Int String -> BST Int String
+removeIf :: (Ord key) => (key -> Bool) -> BST key value -> BST key value
 removeIf _ Empty = Empty -- if empty, return empty (base case)
 removeIf predicate (Node key value left right) -- if not empty, check if the predicate is true
     | predicate key = remove key (Node key value (removeIf predicate left) (removeIf predicate right)) -- if true, remove the node
     | otherwise = Node key value (removeIf predicate left) (removeIf predicate right) -- if false, keep the node
 
 
-treeHeight :: BST a b -> Int
+treeHeight :: BST k v -> Int
+treeHeight Empty = 0
+treeHeight (Node k v left right) = 1 + max (treeHeight left) (treeHeight right)
+-- -1 is used instead of this case so that it contributes to the height of the tree calculation
+-- otherwise, the height of the tree would be 1 less than it actually is
+
+-- Rotate the binary search tree to the left
+rotateLeft :: BST k v -> BST k v
+rotateLeft Empty = Empty
 
 
 -- example tree
